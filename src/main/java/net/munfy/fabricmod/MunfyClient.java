2bd99f347c702e228c2b1a135deb1db97c6b9b84
earthcooo <84674098+earthcooo@users.noreply.github.com>
2022-09-25 15:18:02 +0700

    First

---
+++ src/main/java/net/munfy/fabricmod/MunfyClient.java
@@ -0,0 +1,10 @@
+package net.munfy.fabricmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MunfyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/munfy/fabricmod/MunfyMod.java
@@ -0,0 +1,15 @@
+package net.munfy.fabricmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MunfyMod implements ModInitializer {
+	public static final String MOD_id = "munfymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_id);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
